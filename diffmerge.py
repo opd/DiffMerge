751c674cbe44fdd0ce602c947a9bd8f1fc622549
Andrew <andrewfrommoon@gmail.com>
2020-08-08 21:54:56 +0300

    Diff merge implemented

--- diffmerge.py
+++ diffmerge.py
@@ -1,4 +1,7 @@
 import copy
+import json
+
+from deepdiff import DeepDiff, Delta
 
 def swap_keys(d, rules):
     # Maybe can use deepcopy
@@ -48,9 +51,117 @@ def reverse_diff(diff):
 
 
 class Puppet:
+    def __init__(self, parent=None):
+        # TODO rename parent to root
+        self._parent = parent
+        self._items = {}
+        self._counter = 0
+        self._is_list = False
+        self._is_dict = False
+        self._index = parent.get_index() if parent else 0
+
     def __copy__(self):
         raise NotImplementedError
 
+    def _deepcopy(self, parent=None):
+        obj = Puppet()
+        if parent is None:
+            parent = obj
+        else:
+            obj._parent = parent
+        obj._counter = self._counter
+        obj._is_list = self._is_list
+        obj._is_dict = self._is_dict
+        obj._index = self._index
+        obj._items = {
+            k: v._deepcopy(obj) if isinstance(v, Puppet) else copy.deepcopy(v)
+            for k, v in self._items.items()
+        }
+        return obj
+
     def __deepcopy__(self, memo):
-        # TODO
-        return Puppet()
+        return self._deepcopy()
+
+    def get_item(self):
+        parent = self._parent or self
+        return Puppet(parent=parent)
+
+    def __getitem__(self, key):
+        self.apply_list_fix(key)
+        if key in self._items:
+            return self._items[key]
+        child = self.get_item()
+        self._items[key] = child
+        return child
+
+    def apply_list_fix(self, key):
+        if not isinstance(key, int):
+            self._is_dict = True
+            return
+        self._is_list = True
+        for i in range(key):
+            if i not in self._items:
+                self._items[i] = self.get_item()
+
+    def __setitem__(self, key, value):
+        self.apply_list_fix(key)
+        self._items[key] = value
+        return value
+
+    def __delitem__(self, key):
+        self._items.pop(key, None)
+        if isinstance(key, int):
+            keys = sorted([k for k in self._items.keys() if k > key])
+            for key in keys:
+                self._items[key - 1] = self._items.pop(key)
+
+    def get_index(self):
+        self._counter += 1
+        return self._counter
+
+    # TODO
+    def unique_name(self):
+        return f"puppet_{self._index}"
+
+    def _to_dict(self):
+        def get_value(obj):
+            if isinstance(obj, Puppet):
+                return obj._to_dict()
+            return obj
+
+        if not self._items:
+            if self._is_list:
+                return []
+            if self._is_dict:
+                return {}
+            return self.unique_name()
+
+        keys = None
+        if self._is_list:
+            return [get_value(v) for k, v in sorted(self._items.items(), key=lambda x: x[0])]
+        else:
+            return {
+                key: get_value(value)
+                for key, value in self._items.items()
+            }
+
+    def to_dict(self):
+        return self._to_dict()
+
+    def __str__(self):
+        return str(self.to_dict())
+
+
+def diff_func(t1, t2):
+    return DeepDiff(t1, t2, verbose_level=2)
+
+
+def merge(diffs):
+    obj = Puppet()
+    for diff in diffs:
+        obj = obj + Delta(diff)
+    end_dict = copy.deepcopy(obj.to_dict())
+    for diff in reversed(diffs):
+        obj += Delta(reverse_diff(diff))
+    start_dict = obj.to_dict()
+    return diff_func(start_dict, end_dict)
--- tests/test_diff_merge.py
+++ tests/test_diff_merge.py
@@ -1,8 +1,30 @@
+import copy
+
 from deepdiff import DeepDiff, Delta
 import pytest
 
-from diffmerge import reverse_diff, swap_keys, Puppet
+from diffmerge import reverse_diff, swap_keys, Puppet, merge
 
+# TODO
+DIFF_CHANGES = (
+    (
+        [1, 2, 3],
+        [1, -2, 3],
+        [0, -2, 3],
+        [0, -2, 3, 4],
+        [0, -2, 3, 4, 5, 6],
+        [1, -2, 3, 4, 5, 6],
+    ),
+    (
+        {"x": 1},
+        {"x": 1, "y": [{"a": [1, 2, 3]}]},
+        {"x": 1, "y": [{"a": [1, 2, 3, 4]}]},
+        {"x": 1, "y": [{"a": [2, 3, 4]}]},
+        {"x": 1, "y": [{"a": [2, 3, 4]}], "z": None},
+    ),
+)
+
+# TODO rename
 DIFF_PAIRS = (
     # array
     ([1, 3], [1, 2, 3]),
@@ -19,9 +41,14 @@ DIFF_PAIRS = (
                 "max": 200,
             }]
         }
-
     )
 )
+
+pairs = [
+    (x, y) for chain in DIFF_CHANGES for x, y in zip(chain, chain[1:])
+]
+DIFF_PAIRS = DIFF_PAIRS + tuple(pairs)
+
 
 def diff_func(t1, t2):
     return DeepDiff(t1, t2, verbose_level=2)
@@ -56,6 +83,87 @@ def test_swap_keys():
     rules = [('x', 'y')]
     assert swap_keys(old, rules) == new
 
+def drop_puppet(items):
+    return {
+        key: value for key, value in items.items() if not isinstance(value, Puppet)
+    }
+
+# TODO move to helpers
+class Compare:
+    def __init__(self, func):
+        self._func = func
+
+    def __eq__(self, other):
+        try:
+            return self._func(other)
+        except BaseException:
+            return False
+
+def test_compare():
+    c = Compare(lambda x: isinstance(x, str))
+    assert c == "string"
+    assert c != 1
+
+def test_puppet():
+    obj = Puppet()
+
+    obj["x"] = 3
+    assert obj._items["x"] == 3
+
+    obj["x2"]
+    assert isinstance(obj["x2"], Puppet)
+
+    obj["y"][0] = 10
+    assert obj._items["y"]._items[0] == 10
+
+    obj["y"][10] = 20
+    assert drop_puppet(obj._items["y"]._items) == {0: 10, 10: 20}
+
+    del obj["y"][4]
+    assert drop_puppet(obj._items["y"]._items) == {0: 10, 9: 20}
+
+    data = obj.to_dict()
+    p = Compare(lambda x: x.startswith("puppet_"))
+    assert data == {
+        'x': 3,
+        'x2': p,
+        'y': [
+            10,
+            p,
+            p,
+            p,
+            p,
+            p,
+            p,
+            p,
+            p,
+            20,
+        ],
+    }
+
+
+def test_deep_copy():
+    obj = Puppet()
+    obj["x"] = 1
+    obj_copy = copy.deepcopy(obj)
+    assert obj.to_dict() == obj_copy.to_dict()
+
+    obj = Puppet()
+    obj["x"] = 3
+    obj["y"]
+    obj["y"][0] = 0
+    obj["y"][4] = 40
+
+    obj_copy = copy.deepcopy(obj)
+
+    del obj["y"][1]
+    del obj_copy["y"][1]
+
+    assert obj.to_dict() == obj_copy.to_dict()
+
+    obj["y"][0] = 30
+    assert obj.to_dict() != obj_copy.to_dict()
+
 
 @pytest.mark.parametrize('pair', DIFF_PAIRS)
 @pytest.mark.parametrize('rev', [True, False])
@@ -65,6 +173,25 @@ def test_apply(pair, rev):
     diff = diff_func(pair[0], pair[1])
     obj = Puppet()
     delta = Delta(diff)
-    result = obj + delta
-    # pytest -k test_apply -s -x
-    # assert False
+    end_state = obj + delta
+    end_dict = end_state.to_dict()
+    reversed_diff = reverse_diff(diff)
+    delta_reversed = Delta(reversed_diff)
+    initial_state = end_state + delta_reversed
+
+    initial_dict = initial_state.to_dict()
+    result_diff = diff_func(initial_dict, end_dict)
+    assert result_diff == diff
+
+@pytest.mark.parametrize('chain', DIFF_CHANGES)
+@pytest.mark.parametrize('rev', [False, True])
+def test_merge_diffs(chain, rev):
+    if rev:
+        chain = list(reversed(chain))
+    diffs = []
+    for prev, new in zip(chain, chain[1:]):
+        diffs.append(diff_func(prev, new))
+
+    result_diff = merge(diffs)
+    sample_diff = diff_func(chain[0], chain[-1])
+    assert result_diff == sample_diff
